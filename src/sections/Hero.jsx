f4f42b0f90bbfc1880f597d7c4b32933179203ca
MaheenBashir <maheenbashirr@gmail.com>
2026-05-10 04:42:34 +0500

    Add files via upload

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -72,7 +72,7 @@ export default function Hero() {
               borderRadius: '10px',
               position: 'absolute',
               left: '-80px',
-              bottom: '0',
+              bottom: '60px',
               zIndex: 10,
             }}
           >
